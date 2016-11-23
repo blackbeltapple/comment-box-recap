ad4b13eb4cf99fd124894bb1af09a846a9fea340
amanda manekshaw <amanda_manekshaw@hotmail.com>
2016-11-23 12:55:39 +0000

    Static render complete

--- components/commentform.js
+++ components/commentform.js
@@ -6,7 +6,18 @@ const CommentForm = React.createClass({
   render: function () {
     return (
       <div className='box' >
-        <p>CommentForm</p>
+        <label class="label">Name</label>
+        <p class="control">
+          <input class="input" type="text" placeholder="" />
+        </p>
+        <label class="label">Message</label>
+        <p class="control">
+          <textarea class="textarea" placeholder="Comment here..."></textarea>
+        </p>
+        <p class="control">
+          <button class="button is-primary">Submit</button>
+          <button class="button is-link">Cancel</button>
+        </p>
 
       </div>
     );
